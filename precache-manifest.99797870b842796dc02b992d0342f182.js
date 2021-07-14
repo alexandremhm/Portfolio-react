6ee8bf1394996c24bc58d3e7feefe06903969a74
Matheus_Alexandre <alexandremhm@gmail.com>
2021-07-13 23:01:50 -0300

    Updates

---
+++ precache-manifest.99797870b842796dc02b992d0342f182.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547f6c7ef76983d82b7da5c2cf105b4a",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "d6108a41a61ddc47e688",
+    "url": "/Portfolio-react/static/css/main.a90f24e5.chunk.css"
+  },
+  {
+    "revision": "15faed28c820a08958f1",
+    "url": "/Portfolio-react/static/js/2.8b9c809f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Portfolio-react/static/js/2.8b9c809f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6108a41a61ddc47e688",
+    "url": "/Portfolio-react/static/js/main.c7c7b86d.chunk.js"
+  },
+  {
+    "revision": "139c6ca152dbfc86761c",
+    "url": "/Portfolio-react/static/js/runtime-main.8e29731c.js"
+  },
+  {
+    "revision": "cdb84ed07cfe0435b1ed13b1ddef4fa1",
+    "url": "/Portfolio-react/static/media/cefet.cdb84ed0.png"
+  },
+  {
+    "revision": "fdf247dafc44601544c9ed35f4e40501",
+    "url": "/Portfolio-react/static/media/dev.fdf247da.svg"
+  },
+  {
+    "revision": "a0a4ec316f5e097abac504acca1d5db8",
+    "url": "/Portfolio-react/static/media/logotrybe.a0a4ec31.png"
+  },
+  {
+    "revision": "ff84438d45593c9317a2db14bb5e1189",
+    "url": "/Portfolio-react/static/media/matheus.ff84438d.jpg"
+  }
+]);
